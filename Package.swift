5371abad5c64541ebcc1195438051f16578b149e
noppoman <yuki@miketokyo.com>
2016-05-29 07:17:10 +0900

    Bump Middleware to 0.4

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SessionMiddleware",
     dependencies: [
-        .Package(url: "https://github.com/slimane-swift/Middleware.git", majorVersion: 0, minor: 3),
+        .Package(url: "https://github.com/slimane-swift/Middleware.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/noppoMan/Crypto.git", majorVersion: 0, minor: 3),
         .Package(url: "https://github.com/noppoMan/Suv.git", majorVersion: 0, minor: 5)
     ]
